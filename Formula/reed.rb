e8b7a726dbfad08984ff33b0e7a706abcbd2f723
Adam Tootle <adamtootle@gmail.com>
2026-05-13 19:34:54 -0400

    reed 1.0.1

--- Formula/reed.rb
+++ Formula/reed.rb
@@ -1,18 +1,18 @@
 class Reed < Formula
   desc "Edit markdown locally in your browser"
   homepage "https://github.com/adamtootle/reed"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/adamtootle/reed/releases/download/v1.0.0/reed-macos-arm64.zip"
-      sha256 "8eaa98e9d4da43a674c4c0d59a5ef18abd46e36b992c26e710a0d94211c97dbc"
+      url "https://github.com/adamtootle/reed/releases/download/v1.0.1/reed-macos-arm64.zip"
+      sha256 "e4c92bce805c30bc6e369c808a676ee596e6d8a88a3e730eac178b6cd95a1091"
     end
 
     on_intel do
-      url "https://github.com/adamtootle/reed/releases/download/v1.0.0/reed-macos-x86_64.zip"
-      sha256 "71df282c6d69f88a17770ff29e6cbe8f0e1a3418444200bd0493f0a3e26db8ea"
+      url "https://github.com/adamtootle/reed/releases/download/v1.0.1/reed-macos-x86_64.zip"
+      sha256 "153307f6862cf912336aaf59d63c626d0471274b9bcffeae3ac40dd216c37131"
     end
   end
 
